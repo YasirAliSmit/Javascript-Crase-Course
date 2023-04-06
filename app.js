88dbad07bd6c5d5b4a9c5382f352dd16730e50d6
YasirAliSmit <yasir.ali75404@gmail.com>
2023-04-06 22:56:01 +0500

    myassigments

---
+++ app.js
@@ -0,0 +1,148 @@
+/// Q no 1
+// 1. Write a JavaScript program that accepts two numbers in two prompts and
+// displays the larger one in the console.
+
+// let num1 = 3 ;
+// let num2 = 2 ;
+
+// if ( num1 > num2 ) {
+//     console.log(`Number One  is Greater than num2`)
+// }
+// else {
+
+//     console.log(`Number Two  is Greater than num1`)
+// }
+
+// Q no 2
+
+// let userNumber = 1
+
+// if ( userNumber <= -0 ){
+//     console.log(`Number is Minius -`)
+// }else if ( userNumber >= 0 ){
+//     console.log(`Number is Plus +`)
+// }
+
+// Q no 3
+
+// const userInputOne = 10;
+// const userInputTwo = 1;
+// const userInputThree = 1;
+// const userInputFour = 1;
+// const userInputFive = 18;
+
+// if (!(
+//   userInputOne > userInputTwo ||
+//   userInputOne > userInputThree ||
+//   userInputOne > userInputFour ||
+//   userInputOne > userInputFive)
+// ){
+//     if(!
+// (        userInputTwo > userInputOne ||
+//         userInputTwo > userInputThree ||
+//         userInputTwo > userInputFour ||
+//         userInputTwo > userInputFive)
+//         )
+//         {
+//             if(!(
+//                 userInputThree > userInputOne ||
+//                 userInputThree > userInputTwo ||
+//                 userInputThree > userInputFour ||
+//                 userInputThree > userInputFive)
+//                 ){
+
+//                     if(!(
+//                         userInputFour > userInputOne ||
+//                         userInputFour > userInputTwo ||
+//                         userInputFour > userInputThree ||
+//                         userInputFour > userInputFive)
+//                         ){
+//                             if(
+//                                 userInputFive > userInputOne ||
+//                                 userInputFive > userInputTwo ||
+//                                 userInputFive > userInputThree ||
+//                                 userInputFive > userInputFour){
+
+//                                     console.log(`Num Five is Greater Than ${userInputFive} all Number`)
+//                                 }
+
+//                     }else{
+//                         console.log(`Num Four is Greater Than ${userInputFour} all Number`)
+//                     }
+//                 }else{
+//                     console.log(`Num Three is Greater  ${userInputThree} all Number`)
+//                 }
+//         }
+//         else
+//         {
+//             console.log(`Num Two is Greater  ${userInputTwo} all Number`)
+//         }
+// }else{
+//     console.log(`Num One is Greater Than ${userInputOne} all Number`)
+// }
+
+// Q no 4
+
+// const numCheck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for ( var i = 0 ; i <= numCheck.length ; i++ ) {
+
+//     if (numCheck[i] % 2 == 0) {
+//     console.log(`Its Even Number ${numCheck[i]}`);
+//   }
+//    else {
+//     console.log(`Its Odd Number ${numCheck[i]}`);
+//   }
+
+// }
+  
+
+// Q no 5
+
+// const userGrade = 65
+
+// if (userGrade >= 40 && userGrade <= 60 ){
+//     console.log(`You Got F Grade`)
+// }
+// else if (userGrade >= 61 &&userGrade <= 70 ){
+//     console.log(`You Got D Grade`)
+// }
+// else if (userGrade >= 71 && userGrade <= 80 ){
+//     console.log(`You Got C Grade`)
+// }
+// else if (userGrade >= 81 && userGrade <= 90 ){
+//     console.log(`You Got B Grade`)
+// }
+// else if (userGrade >= 91 && userGrade <= 100 ){
+//     console.log(`You Got A Grade`)
+// }
+
+
+// Q no 6
+
+
+
+
+// Q no 7 
+
+// const stars =['*','*','*','*','*','*','*','*','*','*']
+// for (var i = 0 ; i <= 0 ; i++){
+//     console.log(stars[i])
+//     for (var i = 0 ; i <= 0 ; i++){
+//         console.log(`${stars[i]} ${stars[i]} `)
+//         for (var i = 0 ; i <= 0 ; i++){
+//             console.log(`${stars[i]} ${stars[i]} ${stars[i]}  `)
+//             for (var i = 0 ; i <= 0 ; i++){
+//                 console.log(`${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]}  `)
+//                 for (var i = 0 ; i <= 0 ; i++){
+//                     console.log(`${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]} `)
+//                     for (var i = 0 ; i <= 0 ; i++){
+//                         console.log(`${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]} ${stars[i]}`)
+                       
+//                         }
+//                     }
+//                 }
+//             }
+//             }
+//         }
+    
